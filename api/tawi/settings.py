84b76698b7646b241069838c26861dc206699e18
PeterKahenya <kahenya0@gmail.com>
2023-03-17 21:06:39 +0300

    Added support for static files

--- api/tawi/settings.py
+++ api/tawi/settings.py
@@ -129,16 +129,23 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.environ.get('DJANGO_STATIC_ROOT', "/static/")
+STATIC_ROOT = BASE_DIR / "static"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+from .cdn.conf import *
 
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
